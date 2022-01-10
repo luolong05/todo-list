19e2968e4f2c80af234f06a3e9a05f6456cef8b6
luolong05 <luolong05@163.com>
2022-01-10 20:25:39 +0800

    add index

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/styles/index';
+import GlobalTheme from '@/styles/theme';
+import TodoList from '@/views/todoList';
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <ThemeProvider theme={GlobalTheme}>
+    <GlobalStyle />
+    <React.StrictMode>
+      <TodoList />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );

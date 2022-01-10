50054619df6e500001487e11f3a0b9086397dede
luolong05 <luolong05@163.com>
2022-01-10 19:54:31 +0800

    add global theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colorDefault: "#d9d9d9",
+  colorDefaultActive: "#fff",
+  colorPrimary: "#246ab9",
+  colorPrimaryActive: "#3284e1",
+  colorWarning: "#FF9A2A",
+  colorSuccess: "#7FBC39",
+  colorError: "#F56C6C",
+  colorDisabled: "#ddd",
+  textDefaultColor: '#252323',
+  textDefaultFontSize: '14px',
+  backgroundDefaultColor: '#fff',
+  backgroundDefaultColorActive: '#e8e8e8',
+  bgPrimaryActiveFontColor: '#fff',
+};

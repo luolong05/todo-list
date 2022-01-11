556726c8da754fd50902af0fc23926dc232b9870
luolong05 <luolong05@163.com>
2022-01-11 15:12:33 +0800

    render TodoManager

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@/styles/index';
 import GlobalTheme from '@/styles/theme';
-import TodoList from '@/views/todoList';
+import TodoManager from '@/views/todoList';
 
 ReactDOM.render(
   <ThemeProvider theme={GlobalTheme}>
     <GlobalStyle />
     <React.StrictMode>
-      <TodoList />
+      <TodoManager />
     </React.StrictMode>
   </ThemeProvider>,
   document.getElementById('root')

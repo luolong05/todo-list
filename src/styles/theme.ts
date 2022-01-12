ed2e49d69cec2c329576608947f898256bb5f893
luolong05 <luolong05@163.com>
2022-01-12 09:10:00 +0800

    delete unuse css;

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,16 +1,11 @@
 export default {
-  colorDefault: "#505050",
-  colorDefaultActive: "#fff",
-  colorPrimary: "#246ab9",
-  colorPrimaryActive: "#3284e1",
-  colorWarning: "#FF9A2A",
-  colorSuccess: "#30c557",
-  colorError: "#F56C6C",
-  colorDisabled: "#ddd",
+  colorDefault: '#505050',
+  colorDefaultActive: '#fff',
+  colorPrimary: '#246ab9',
+  colorPrimaryActive: '#3284e1',
+  colorSuccess: '#30c557',
+  colorDisabled: '#ddd',
   textDefaultColor: '#252323',
   textDefaultFontSize: '14px',
-  textLgFontSize: '18px',
-  backgroundDefaultColor: '#fff',
-  backgroundDefaultColorActive: '#e8e8e8',
-  bgPrimaryActiveFontColor: '#fff',
+  textLgFontSize: '18px'
 };

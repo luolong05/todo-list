f601ab4a48ffa0027b0fc8586f3982c8bb498b70
luolong05 <luolong05@163.com>
2022-01-12 09:09:40 +0800

    delete unuse css;

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -14,77 +14,13 @@ html, body {
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
   margin: 0;
+  padding: 0;
   outline: none;
   box-shadow: none;
+  border: none;
 }
 
 ul, li {
-  margin: 0;
-  padding: 0;
   list-style: none;
-}
-
-p {
-  margin: 0;
-  padding: 0;
-}
-
-img {
-  border: none;
-}
-
-input, button, select, textarea {
-  outline: none;
-  border: none;
-}
-
-input:focus, textarea:focus {
-  border-color: #fff;
-  outline: none !important;
-}
-
-a {
-  text-decoration: none;
-  outline: none;
-  color: #fff;
-  &:hover,
-  &:active,
-  &:focus {
-    text-decoration: none;
-    outline: none;
-  }
-  &[disabled] {
-    cursor: not-allowed;
-    pointer-events: none;
-  }
-}
-
-button,
-button:focus,
-button:active,
-button:hover {
-  outline: none;
-}
-
-button::-moz-focus-inner,
-input[type="reset"]::-moz-focus-inner,
-input[type="button"]::-moz-focus-inner,
-input[type="submit"]::-moz-focus-inner,
-input[type="file"] > input[type="button"]::-moz-focus-inner {
-  border: none
-}
-
-input::-webkit-contacts-auto-fill-button {
-  visibility: hidden;
-  display: none !important;
-  pointer-events: none;
-  position: absolute;
-  right: 0;
-}
-
-input:-webkit-autofill,
-input:-webkit-autofill:hover,
-input:-webkit-autofill:focus {
-  -webkit-box-shadow: 0 0 0 1000px white inset !important;
 }
 `;

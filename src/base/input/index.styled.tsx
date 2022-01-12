ae2e950478bb9a15ab0ffabc3237a59b14b395b4
luolong05 <luolong05@163.com>
2022-01-12 09:10:39 +0800

    update background-color

--- src/base/input/index.styled.tsx
+++ src/base/input/index.styled.tsx
@@ -4,7 +4,7 @@ export const StyledInput = styled.input`
   border: 1px solid ${(props) => props.theme.colorDefault};
   padding: 10px 14px;
   width: 100%;
-  background-color: ${(props) => props.theme.backgroundDefaultColor};
+  background-color: ${(props) => props.theme.colorDefaultActive};
   color: ${(props) => props.theme.textDefaultColor};
   font-size: ${(props) => props.theme.textLgFontSize};
   border-radius: 4px;
